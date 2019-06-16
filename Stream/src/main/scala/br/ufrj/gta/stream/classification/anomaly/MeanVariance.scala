abd09dab7cc4de96180d501c712b7d08b581e7f6
alfabr90 <alfabr90@gmail.com>
2019-06-16 16:04:35 -0300

    Create traits of shared params by the classifiers

--- Stream/src/main/scala/br/ufrj/gta/stream/classification/anomaly/MeanVariance.scala
+++ Stream/src/main/scala/br/ufrj/gta/stream/classification/anomaly/MeanVariance.scala
@@ -2,31 +2,21 @@ package br.ufrj.gta.stream.classification.anomaly
 
 import org.apache.spark.ml.linalg.{Vector, Vectors}
 import org.apache.spark.ml.param._
-import org.apache.spark.ml.param.shared._
 import org.apache.spark.ml.{Predictor, PredictionModel}
 import org.apache.spark.ml.stat.Summarizer
 import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.sql.{Dataset, DataFrame}
 import org.apache.spark.sql.functions._
 
+import br.ufrj.gta.stream.param._
+
 case class FeaturesMeanVariance(mean: Array[Double], variance: Array[Double])
 
 case class MeanVarianceLimits(lower: Array[Double], upper: Array[Double])
 
-private[classification] trait MeanVarianceClassifierParams extends Params {
-    var threshold = new DoubleParam(this, "threshold", "threshold parameter (>= 0)")
-
-    def getThreshold: DoubleParam = this.threshold
-
-    def setThreshold(value: Double): this.type = {
-        require(value >= 0.0, s"Threshold value must not be negative")
-        set(this.threshold, value)
-    }
-}
-
 class MeanVarianceClassifier(override val uid: String)
     extends Predictor[Vector, MeanVarianceClassifier, MeanVarianceModel]
-    with MeanVarianceClassifierParams {
+    with HasThresholdParams {
 
     def this() = this(Identifiable.randomUID("mvc"))
 
@@ -66,7 +56,7 @@ private[stream] class MeanVarianceModel(
         val limits: MeanVarianceLimits)
     extends PredictionModel[Vector, MeanVarianceModel] {
 
-    require(this.limits.lower.size == this.limits.upper.size, s"The sizes of lower and upper limits Vectors must be the equal")
+    require(this.limits.lower.size == this.limits.upper.size, s"The sizes of lower and upper limits arrays must be equal")
 
     override val numFeatures = this.limits.lower.size
 
@@ -77,8 +67,8 @@ private[stream] class MeanVarianceModel(
     override def predict(features: Vector): Double = {
         var i = 0
 
-        for (i <- 1 to this.numFeatures) {
-            if (features(i - 1) < this.limits.lower(i - 1) || features(i - 1) > this.limits.upper(i - 1)) {
+        for (i <- 0 until this.numFeatures) {
+            if (features(i) < this.limits.lower(i) || features(i) > this.limits.upper(i)) {
                 return 1.0
             }
         }

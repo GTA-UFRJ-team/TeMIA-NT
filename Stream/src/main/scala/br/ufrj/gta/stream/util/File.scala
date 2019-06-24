9391d927efceec445ba6937be58d98073228494f
alfabr90 <alfabr90@gmail.com>
2019-06-23 22:09:02 -0300

    Implement additional utility methods for filesystem operations

--- Stream/src/main/scala/br/ufrj/gta/stream/util/File.scala
+++ Stream/src/main/scala/br/ufrj/gta/stream/util/File.scala
@@ -1,10 +1,35 @@
 package br.ufrj.gta.stream.util
 
 import java.io._
+import java.nio.file.{Files, Paths}
 
 object File {
+    def appendSlash(path: String): String = {
+        if (!path.endsWith("/")) {
+            path + "/"
+        } else {
+            path
+        }
+    }
+
+    def getPathnameFromFilename(filename: String): String = {
+        filename.substring(0, filename.lastIndexOf("/"))
+    }
+
+    @throws(classOf[Exception])
+    def createDirIfNotExists(path: String): Unit = {
+        val p = Paths.get(path)
+
+        if (!Files.exists(p)) {
+            Files.createDirectories(p)
+        }
+    }
+
     @throws(classOf[Exception])
     def getFileWriter(filename: String, encoding: String): BufferedWriter = {
+        val path = File.getPathnameFromFilename(filename)
+
+        File.createDirIfNotExists(path)
         new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename), encoding))
     }
 
